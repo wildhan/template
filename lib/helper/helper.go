c5731d3b4cad6d8607e8707ebff821187c40ac9c
wildhan <wildhan.ibrahim@gmail.com>
2025-05-25 21:57:41 +0700

    feat: init func HashPassword() and CheckPasswordHash() in helper

--- lib/helper/helper.go
+++ lib/helper/helper.go
@@ -1,5 +1,7 @@
 package helper
 
+import "golang.org/x/crypto/bcrypt"
+
 func EmptyStringToNull(input string) interface{} {
 	if input != "" {
 		return input
@@ -7,4 +9,16 @@ func EmptyStringToNull(input string) interface{} {
 		return nil
 	}
 
+}
+
+// Fungsi untuk mengenkripsi password
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+// Fungsi untuk membandingkan password dengan hash-nya
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
